b80a40e6b037666d8878b16baf4bc91ab1a5a46c
Lucas Iankoski <lucas@digihub.com.br>
2023-09-13 19:47:48 -0300

    Feat: Criação da Entidade e Repository Componente.

---
+++ src/main/java/com/vettorello/avaliacoes/entities/Componente.java
@@ -0,0 +1,22 @@
+package com.vettorello.avaliacoes.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Table(name = "avaliacao", schema = "vettorello")
+public class Componente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+}
