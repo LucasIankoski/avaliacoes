1fa4aa4d3f8e6a65f8bcc8f06c9f1f6a021f862a
Lucas Iankoski <lucas@digihub.com.br>
2023-09-13 20:22:17 -0300

    Feat: método para salvar componente.

---
+++ src/main/java/com/vettorello/avaliacoes/services/ComponenteService.java
@@ -0,0 +1,19 @@
+package com.vettorello.avaliacoes.services;
+
+import com.vettorello.avaliacoes.entities.Componente;
+import com.vettorello.avaliacoes.repositories.ComponenteRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ComponenteService {
+
+    @Autowired
+    private ComponenteRepository repository;
+
+    @Transactional
+    public Componente salvar(Componente componente){
+        return repository.save(componente);
+    }
+}
